049ba0c8eb20b15124d9dbfcb38f27ab283fa294
Andrea Palma <ndrp@Andreas-MacBook-Pro.local>
2021-03-03 13:16:24 +0100

    modifiche production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,15 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_APP_PASSWORD"]
+  }
   config.action_mailer.default_url_options = { host: "yourapp.herokuapp.com" }
 
 
