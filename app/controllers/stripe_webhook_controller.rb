d1bca8f604be573ccf3fd0d30a5d66379c0fccac
Andrea Palma <ndrp@Andreas-MacBook-Pro.local>
2021-03-04 17:10:40 +0100

    ultime modifiche 2.0

--- app/controllers/stripe_webhook_controller.rb
+++ app/controllers/stripe_webhook_controller.rb
@@ -1,26 +1,7 @@
 require 'json'
-require 'create_transaction_record'
-require 'send_payment_succeeded_mail'
-require 'send_payment_failed_mail'
 
 class StripeWebhookController < ApplicationController
   def stripe
 
-    response = JSON.parse(request.body.read)
-    data_object = response["object"]
-
-    case response["type"]
-    when "checkout.session.completed"
-  
-      create_transaction = CreateTransactionRecord.new.call(data_object)
-
-      send_mail_notification = SendPaymentSucceededMail.new.call(data_object)
-
-      return head :ok if create_transaction.success? && send_mail_notification.success?
-
-    else
-      send_mail_notification = SendPaymentFailedMail.new.call(data_object)
-      return head :ok if send_mail_notification.success?
-    end
   end
 end
