16b9f379d2f0eeb3a274d2168fc0388b3601b61d
Andrea Palma <ndrp@Andreas-MacBook-Pro.local>
2021-08-31 21:06:47 +0200

    method changed

--- app/services/stripe_checkout_session_increment_premium_service.rb
+++ app/services/stripe_checkout_session_increment_premium_service.rb
@@ -2,7 +2,7 @@ class StripeCheckoutSessionIncrementPremiumService
     def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
     user = User.find(order.user_id)
-    user.premium_until = Date.now + 1.year
+    user.premium_until = Time.now + 1.year
     user.save
     end
 end
